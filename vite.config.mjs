7229b10a7f2f9ad6d1b52dc4bea3846072f8a427
illysito <illyalukyanovdesign@gmail.com>
2025-11-01 15:06:57 +0000

    Update vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -16,23 +16,23 @@ export default defineConfig({
   build: {
     minify: true,
     manifest: true,
-    rollupOptions: {
-      input: './src/main.js',
-      external: ['jquery'], // ✅ keep this
-      // 👈 no custom output here
-    },
     // rollupOptions: {
     //   input: './src/main.js',
-    //   output: {
-    //     format: 'umd',
-    //     entryFileNames: 'main.js',
-    //     esModule: false,
-    //     compact: true,
-    //     globals: {
-    //       jquery: '$',
-    //     },
-    //   },
-    //   external: ['jquery'],
+    //   external: ['jquery'], // ✅ keep this
+    //   // 👈 no custom output here
     // },
+    rollupOptions: {
+      input: './src/main.js',
+      output: {
+        format: 'umd',
+        entryFileNames: 'main.js',
+        esModule: false,
+        compact: true,
+        globals: {
+          jquery: '$',
+        },
+      },
+      external: ['jquery'],
+    },
   },
 })
