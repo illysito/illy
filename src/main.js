497f69b018a4bc9644c25bc65e4200bd433cdbb1
illysito <illyalukyanovdesign@gmail.com>
2025-11-01 15:40:47 +0000

    Update main.js

--- src/main.js
+++ src/main.js
@@ -73,7 +73,7 @@ async function runHomeFunctions() {
   const { default: serviceAnimations } = await import(
     './features/pages/home/service_animations'
   )
-  const { default: golUI } = await import('./features/p5js/game_of_life/gol_ui')
+  // const { default: golUI } = await import('./features/p5js/game_of_life/gol_ui')
 
   // Hero
   heroUI()
