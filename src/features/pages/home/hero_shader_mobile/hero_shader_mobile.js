3bcffe469812e2786c16b00eb401c6b469cc8f2d
illysito <illyalukyanovdesign@gmail.com>
2025-11-05 21:09:02 +0000

    Update hero_shader_mobile.js

--- src/features/pages/home/hero_shader_mobile/hero_shader_mobile.js
+++ src/features/pages/home/hero_shader_mobile/hero_shader_mobile.js
@@ -97,7 +97,7 @@ void main()
 
       // controlled distortions
 
-  float accelerationScroll = u_acceleration;
+  float accelerationScroll = displacement.r * 0.02 * u_acceleration;
 
       // weather displacement
 
