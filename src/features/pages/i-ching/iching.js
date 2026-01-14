f5ab4a566c9492bf09d9a667e7783cac603dda9b
illysito <illyalukyanovdesign@gmail.com>
2026-01-14 14:00:36 +0000

    Update iching.js

--- src/features/pages/i-ching/iching.js
+++ src/features/pages/i-ching/iching.js
@@ -51,20 +51,21 @@ function iChing() {
   let isBlocked = false
 
   function blockButton() {
-    gsap.to(coinButton, {
-      // opacity: 0.4,
-      pointerEvents: 'none',
-      duration: 0.2,
-    })
-    isBlocked = true
-    setTimeout(() => {
-      gsap.to(coinButton, {
-        // opacity: 1,
-        pointerEvents: 'auto',
-        duration: 0.2,
-      })
-      isBlocked = false
-    }, 3400)
+    // gsap.to(coinButton, {
+    //   // opacity: 0.4,
+    //   pointerEvents: 'none',
+    //   duration: 0.2,
+    // })
+    // isBlocked = true
+    // setTimeout(() => {
+    //   gsap.to(coinButton, {
+    //     // opacity: 1,
+    //     pointerEvents: 'auto',
+    //     duration: 0.2,
+    //   })
+    //   isBlocked = false
+    // }, 3400)
+    console.log('off for a while')
   }
 
   function init() {
