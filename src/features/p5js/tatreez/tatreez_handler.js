b2b8319329f778e314e5513b47641d544c3d65fb
illysito <illyalukyanovdesign@gmail.com>
2026-01-21 18:24:01 +0000

    :)

--- src/features/p5js/tatreez/tatreez_handler.js
+++ src/features/p5js/tatreez/tatreez_handler.js
@@ -10,13 +10,17 @@ function tatreezHandler(canvasWrapper) {
     let canvasParent
     let width
     let height
-    // let cols = 30
-    // let rows = 2 * cols / 3
     let res = 16
     let cols
     let rows
-
+    let currentArr = []
     let index = 0
+    let startIndex = 0
+
+    // RANDOM TRANSITION
+    let pending = []
+    let transitionSpeed = 12
+    let transitionLowerBound = 8
 
     sk.setup = () => {
       // Define parent and dimensions, create Canvas and attach it to the parent using its dimensions. ALWAYS in JS
@@ -26,30 +30,39 @@ function tatreezHandler(canvasWrapper) {
       const c = sk.createCanvas(width, height)
       c.parent(canvasParent)
 
-      sk.background(sk.color(255, 252, 249))
+      // sk.background(sk.color(255, 252, 249))
+      // sk.stroke(sk.color(252, 252, 249))
+      sk.background(sk.color(16))
       sk.stroke(sk.color(16))
-      sk.strokeWeight(0.2)
+      sk.fill(252, 104, 104)
+      sk.strokeWeight(0.3)
       sk.fill(sk.color(16))
 
       cols = Math.floor(width / res)
       rows = Math.floor(height / res)
+
+      // init currentArr
+      for (let i = 0; i < cols; i++) {
+        currentArr[i] = []
+        pending[i] = []
+        for (let j = 0; j < rows; j++) {
+          currentArr[i][j] = 0
+          pending[i][j] = 0
+        }
+      }
+
+      sk.frameRate(30)
     }
 
     sk.draw = () => {
-      sk.background(255, 252, 249)
-      let arr = tatreezArray[index]
-      for (let i = 0; i < cols; i++) {
-        for (let j = 0; j < rows; j++) {
-          if (arr[i][j] == 1) {
-            sk.fill(16)
-          } else if (arr[i][j] == 2) {
-            sk.fill(0, 0, 255)
-          } else {
-            sk.fill(255, 252, 249)
-          }
-          sk.rect(i * res, j * res, res, res)
-        }
-      }
+      // sk.background(255, 252, 249)
+      let targetArr = tatreezArray[index]
+
+      // mutate current array based on target array
+      mutateCurrentArrayRandom(currentArr, targetArr)
+
+      // draw current array
+      drawPattern(currentArr)
     }
 
     sk.windowResized = () => {
@@ -59,10 +72,118 @@ function tatreezHandler(canvasWrapper) {
       sk.resizeCanvas(width, height)
     }
 
-    window.addEventListener('keydown', (e) => {
-      if (e.key === 'Enter') {
-        index = (index + 1) % tatreezArray.length
+    function initPending() {
+      for (let i = 0; i < cols; i++) {
+        pending[i] = []
+        for (let j = 0; j < rows; j++) {
+          pending[i][j] = 0
+        }
       }
+    }
+
+    function mutateCurrentArrayByColumn(currentArr, targetArr) {
+      // console.log(startIndex)
+      for (let i = startIndex; i < startIndex + 1; i++) {
+        for (let j = 0; j < rows; j++) {
+          // console.log(i, j)
+          currentArr[i][j] = targetArr[i][j]
+        }
+      }
+      if (startIndex <= 18) {
+        startIndex++
+      }
+    }
+
+    function mutateCurrentArrayByRow(currentArr, targetArr) {
+      // console.log(startIndex)
+      for (let i = 0; i < cols; i++) {
+        for (let j = startIndex; j < startIndex + 1; j++) {
+          // console.log(i, j)
+          currentArr[i][j] = targetArr[i][j]
+        }
+      }
+      if (startIndex <= 18) {
+        startIndex++
+      }
+    }
+
+    function mutateCurrentArrayRandom(currentArr, targetArr) {
+      // Update pending
+      for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+          if (currentArr[i][j] != targetArr[i][j]) {
+            pending[i][j] = 1
+          } else {
+            pending[i][j] = 0
+          }
+        }
+      }
+
+      for (let i = 0; i < transitionSpeed; i++) {
+        for (let j = 0; j < transitionSpeed; j++) {
+          let randomI = Math.floor(sk.random(21))
+          let randomJ = Math.floor(sk.random(21))
+          if (pending[randomI][randomJ] == 1) {
+            currentArr[randomI][randomJ] = targetArr[randomI][randomJ]
+          }
+        }
+      }
+
+      if (transitionSpeed != transitionLowerBound) {
+        transitionSpeed--
+      }
+    }
+
+    function drawPattern(currentArr) {
+      for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+          if (currentArr[i][j] != 0) {
+            fillSquare(currentArr[i][j])
+          } else {
+            eraseSquare()
+          }
+          sk.rect(j * res, i * res, res, res)
+        }
+      }
+    }
+
+    function fillSquare(color) {
+      if (color == 1) {
+        // red
+        sk.fill(252, 104, 104)
+      } else if (color == 2) {
+        // blue
+        sk.fill(120, 133, 255)
+      } else if (color == 3) {
+        // pink
+        sk.fill(255, 174, 220)
+      } else if (color == 4) {
+        // green
+        sk.fill(92, 183, 177)
+      } else if (color == 5) {
+        // yellow
+        sk.fill(255, 161, 67)
+      }
+    }
+
+    function eraseSquare() {
+      // sk.fill(255, 252, 249)
+      sk.fill(16)
+    }
+
+    window.addEventListener('right-click', () => {
+      initPending()
+      startIndex = 0
+      transitionSpeed = 12
+      index = (index + 1) % tatreezArray.length
+    })
+
+    window.addEventListener('left-click', () => {
+      initPending()
+      startIndex = 0
+      transitionSpeed = 12
+      index = index - 1
+      if (index < 0) index = tatreezArray.length - 1
     })
   })
 }
--- src/features/p5js/tatreez/tatreez_ui.js
+++ src/features/p5js/tatreez/tatreez_ui.js
@@ -1,13 +1,119 @@
-// import gsap from 'gsap'
+import gsap from 'gsap'
 
-// import Anim from '../../helpers/anim.js'
+import Anim from '../../helpers/anim.js'
 
-// const D = Anim.D
-// const E = Anim.E
+const D = Anim.D
+const E = Anim.E
 
 async function tatreezUI() {
   const { default: tatreezHandler } = await import('./tatreez_handler.js')
   const tatreezCanvasWrapper = document.querySelector('.tatreez-canvas-wrapper')
+
+  const arrowLeft = document.querySelector('.arrow-left')
+  const arrowRight = document.querySelector('.arrow-right')
+  const tatreezHeaders = document.querySelectorAll('.tatreez-arabic-h')
+  const tatreezTxts = document.querySelectorAll('.tatreez-explanation')
+  const LENGTH = 3
+
+  let counter = 0
+
+  function moveHeaders() {
+    gsap.to(tatreezHeaders, {
+      yPercent: -100 * counter,
+      duration: D.slow,
+      ease: E.eio,
+    })
+  }
+
+  function moveTexts() {
+    gsap.to(tatreezTxts, {
+      yPercent: -100 * counter,
+      duration: D.slow,
+      ease: E.eio,
+    })
+  }
+
+  const rightClick = new CustomEvent('right-click')
+  const leftClick = new CustomEvent('left-click')
+
+  function animateClick(button) {
+    gsap.to(button, {
+      scale: 0.92,
+      duration: D.fast,
+      ease: E.linear,
+      onComplete: () => {
+        gsap.to(button, {
+          scale: 0.96,
+          duration: D.fast,
+          ease: E.linear,
+        })
+      },
+    })
+  }
+
+  arrowRight.addEventListener('mouseover', () => {
+    gsap.to(arrowRight, {
+      scale: 0.96,
+      // x: 2,
+      duration: D.fast,
+    })
+  })
+
+  arrowRight.addEventListener('mouseleave', () => {
+    gsap.to(arrowRight, {
+      scale: 1,
+      x: 0,
+      duration: D.fast,
+    })
+  })
+
+  arrowLeft.addEventListener('mouseover', () => {
+    gsap.to(arrowLeft, {
+      scale: 0.96,
+      // x: -2,
+      duration: D.fast,
+    })
+  })
+
+  arrowLeft.addEventListener('mouseleave', () => {
+    gsap.to(arrowLeft, {
+      scale: 1,
+      x: 0,
+      duration: D.fast,
+    })
+  })
+
+  arrowRight.addEventListener('click', () => {
+    animateClick(arrowRight)
+    window.dispatchEvent(rightClick)
+    counter = (counter + 1) % LENGTH
+    moveHeaders()
+    moveTexts()
+  })
+
+  arrowLeft.addEventListener('click', () => {
+    animateClick(arrowLeft)
+    window.dispatchEvent(leftClick)
+    counter = counter - 1
+    if (counter < 0) counter = LENGTH - 1
+    moveHeaders()
+    moveTexts()
+  })
+
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'ArrowRight') {
+      window.dispatchEvent(rightClick)
+      counter = (counter + 1) % LENGTH
+      moveHeaders()
+    }
+
+    if (e.key === 'ArrowLeft') {
+      window.dispatchEvent(leftClick)
+      counter = counter - 1
+      if (counter < 0) counter = LENGTH - 1
+      moveHeaders()
+    }
+  })
 
   tatreezHandler(tatreezCanvasWrapper)
 }
