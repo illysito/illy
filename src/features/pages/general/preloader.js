585a1678e19fa2512185f3533cdd78be2862399c
illysito <illyalukyanovdesign@gmail.com>
2026-01-28 14:04:26 +0000

    Update preloader.js

--- src/features/pages/general/preloader.js
+++ src/features/pages/general/preloader.js
@@ -3,13 +3,8 @@ import gsap from 'gsap'
 import introHome from '../../pages/home/introHome'
 
 async function preloader() {
-  let isMobile = window.innerWidth <= 767
-  let imgX
-  if (isMobile) {
-    imgX = 60
-  } else {
-    imgX = 160
-  }
+  // let isMobile = window.innerWidth <= 767
+
   document.body.classList.add('no-scroll')
 
   // console.log('running preloader')
@@ -21,248 +16,69 @@ async function preloader() {
 
   function domElementsQuery() {
     return {
-      // preload
-      preloaderText: document.querySelector('.percentage-block-p'),
       preloader: document.querySelector('.preloader__section'),
       preloaderSentence1: document.querySelector('.preloader-text-1'),
       preloaderSentence2: document.querySelector('.preloader-text-2'),
-      preloaderUnderscore: document.querySelector('.underscore.is--preloader'),
       word1: document.querySelector('.preloader-word-1'),
       word2: document.querySelector('.preloader-word-2'),
-      imgWrapper: document.querySelector('.preloader-img-wrapper'),
-      curtain1: document.querySelector('.preloader-curtain-1'),
-      curtain2: document.querySelector('.preloader-curtain-2'),
+      columns: document.querySelectorAll('.preloader-column'),
     }
   }
   const domElements = domElementsQuery()
 
-  // let countIndex = 0
-  // let countChar = ''
-  // function randomChar() {
-  //   //prettier-ignore
-  //   const chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWXxYyZzáéíóú0123456789'
-  //   // const chars = '0123456789'
-  //   countChar = chars[countIndex]
-  //   countIndex = (countIndex + 1) % 62
-  // }
-
-  // let isGeneratingName = false
-  // async function generateName(target) {
-  //   return new Promise((resolve) => {
-  //     if (isGeneratingName) return
-  //     isGeneratingName = true
-
-  //     const targetText = target
-  //     const targetChars = targetText.split('')
-  //     const time = 32
-
-  //     const names = new Array(targetChars.length).fill(' ')
-  //     names[0] = targetChars[0]
-  //     let index = 1
-
-  //     const interval = setInterval(() => {
-  //       if (index < targetChars.length) {
-  //         if (names[index] !== targetChars[index]) {
-  //           randomChar()
-  //           names[index] = countChar
-  //           // if (isUpperCase(targetChars[index])) {
-  //           //   names[index] = targetChars[index]
-  //           // }
-  //           if (countIndex == 24) {
-  //             names[index] = targetChars[index]
-  //           }
-  //         } else {
-  //           countIndex = 0
-  //           index++ // Move to the next character only when correct
-  //         }
-
-  //         domElements.preloaderText.textContent = names.join('')
-
-  //         // console.log(names.join(''))
-  //       } else {
-  //         clearInterval(interval)
-  //         gsap.to(domElements.preloaderSentence1, {
-  //           yPercent: 0,
-  //           duration: 1.2 * dur,
-  //           ease: 'power.inOut',
-  //         })
-  //         gsap.to(
-  //           [domElements.preloaderSentence2, domElements.preloaderUnderscore],
-  //           {
-  //             yPercent: 0,
-  //             stagger: -0.2,
-  //             duration: 1.2 * dur,
-  //             ease: 'power.inOut',
-  //           }
-  //         )
-  //         gsap.to(domElements.imgWrapper, {
-  //           opacity: 0,
-  //           duration: 1.2 * dur,
-  //           ease: 'power.inOut',
-  //         })
-  //         gsap.to(domElements.preloaderText, {
-  //           opacity: 0,
-  //           duration: 1.2 * dur,
-  //           ease: ease2,
-  //           // delay: 1,
-  //           onComplete: () => {
-  //             localStorage.setItem('preloaderShown', 'true')
-  //             resolve()
-  //           },
-  //         })
-  //         isGeneratingName = false
-  //         // console.log('Match found:', names.join(''))
-  //       }
-  //     }, time)
-  //   })
-  // }
-
-  // async function generateName(target) {
-  //   return new Promise((resolve) => {
-  //     const targetChars = target.split('')
-  //     const names = new Array(targetChars.length).fill(' ')
-  //     names[0] = targetChars[0]
-  //     let index = 1
-
-  //     function step() {
-  //       if (index < targetChars.length) {
-  //         randomChar()
-  //         names[index] = countChar
-  //         if (countIndex === 32) {
-  //           names[index] = targetChars[index]
-  //           index++
-  //         }
-  //         domElements.preloaderText.textContent = names.join('')
-  //         requestAnimationFrame(step) // next frame
-  //       } else {
-  //         // end of animation
-  //         gsap.to(domElements.preloaderSentence1, {
-  //           yPercent: 0,
-  //           duration: 1.2 * dur,
-  //           ease: 'power.inOut',
-  //         })
-  //         gsap.to(
-  //           [domElements.preloaderSentence2, domElements.preloaderUnderscore],
-  //           {
-  //             yPercent: 0,
-  //             stagger: -0.2,
-  //             duration: 1.2 * dur,
-  //             ease: 'power.inOut',
-  //           }
-  //         )
-  //         gsap.to(domElements.imgWrapper, {
-  //           opacity: 0,
-  //           duration: 1.2 * dur,
-  //           ease: 'power.inOut',
-  //         })
-  //         gsap.to(domElements.preloaderText, {
-  //           opacity: 0,
-  //           duration: 1.2 * dur,
-  //           ease: ease2,
-  //           // delay: 1,
-  //           onComplete: () => {
-  //             localStorage.setItem('preloaderShown', 'true')
-  //             resolve()
-  //           },
-  //         })
-  //         // resolve()
-  //       }
-  //     }
-
-  //     requestAnimationFrame(step)
-  //   })
-  // }
-
-  function fadeElements() {
-    gsap.to(domElements.preloaderSentence1, {
-      yPercent: 0,
-      duration: 1.2 * dur,
-      ease: 'power.inOut',
-    })
-    gsap.to([domElements.preloaderSentence2, domElements.preloaderUnderscore], {
-      yPercent: 0,
-      stagger: -0.2,
-      duration: 1.2 * dur,
-      ease: 'power.inOut',
-    })
-    gsap.to(domElements.imgWrapper, {
-      opacity: 0,
-      duration: 1.2 * dur,
-      ease: 'power.inOut',
-      onComplete: () => {
-        localStorage.setItem('preloaderShown', 'true')
-        // resolve()
-      },
-    })
-  }
-
-  function fadePreloader() {
-    gsap.to(domElements.preloader, {
-      delay: delay,
-      yPercent: -100,
-      duration: 1.2,
-      ease: 'expo.inOut',
-      onComplete: () => {
-        gsap.set(domElements.preloader, { zIndex: -30 })
-      },
-    })
-  }
-
-  function animateWords() {
+  function animateWordsIn() {
     // TWEEN INITIALS
     gsap.to(domElements.preloaderSentence1, {
       delay: 2 * delay,
       yPercent: -100,
       opacity: 1,
       duration: 1.2 * dur,
-      ease: 'power.inOut',
+      ease: 'power1.inOut',
     })
-    gsap.to([domElements.preloaderSentence2, domElements.preloaderUnderscore], {
+    gsap.to(domElements.preloaderSentence2, {
       delay: 2 * delay,
       yPercent: 100,
       opacity: 1,
-      stagger: -0.2,
       duration: 1.2 * dur,
-      ease: 'power.inOut',
+      ease: 'power1.inOut',
     })
-    gsap.to(domElements.word1, {
-      delay: 3 * delay,
-      x: -imgX,
-      duration: dur,
-      ease: 'power.inOut',
-    })
-    gsap.to(domElements.word2, {
-      delay: 3 * delay,
-      x: imgX,
-      duration: dur,
-      ease: 'power.inOut',
-    })
-    // gsap.to(domElements.preloaderText, {
-    //   delay: 2 * delay,
-    //   opacity: 1,
-    //   duration: dur,
-    //   ease: 'power.inOut',
-    // })
   }
 
-  function animateImg() {
-    gsap.from(domElements.imgWrapper, {
-      delay: delay,
-      scale: 0.8,
-      duration: 2 * dur,
-      ease: 'power2.inOut',
+  function fadeElementsOut() {
+    gsap.to(domElements.preloaderSentence1, {
+      yPercent: -200,
+      duration: 1.2 * dur,
+      ease: 'power1.inOut',
     })
-    gsap.to(domElements.curtain1, {
-      delay: delay,
-      yPercent: -100,
-      duration: 1.5 * dur,
-      ease: 'power3.inOut',
+    gsap.to(domElements.preloaderSentence2, {
+      yPercent: 200,
+      // stagger: -0.2,
+      duration: 1.2 * dur,
+      ease: 'power1.inOut',
     })
-    gsap.to(domElements.curtain2, {
-      delay: delay,
-      yPercent: 100,
-      duration: 1.5 * dur,
+  }
+
+  function fadePreloaderOut() {
+    let fadeDirection
+    const directionBias = Math.random()
+    if (directionBias < 0.5) {
+      fadeDirection = 1
+    } else {
+      fadeDirection = -1
+    }
+    gsap.to(domElements.columns, {
+      // delay: delay,
+      yPercent: 100 * fadeDirection,
+      // opacity: 0,
+      stagger: {
+        each: 0.01,
+        from: 'random',
+      },
+      duration: 1,
       ease: 'power3.inOut',
+      onComplete: () => {
+        gsap.set(domElements.preloader, { zIndex: -30 })
+      },
     })
   }
 
@@ -279,14 +95,13 @@ async function preloader() {
 
   // INIT
   async function init() {
-    animateImg()
-    animateWords()
+    animateWordsIn()
     // await generateName('100')
     setTimeout(() => {
-      fadeElements()
-    }, 2400)
+      fadeElementsOut()
+    }, 2000)
     setTimeout(() => {
-      fadePreloader()
+      fadePreloaderOut()
     }, 2800)
     setTimeout(() => {
       fadeMotionButton()
